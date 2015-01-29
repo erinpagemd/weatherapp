e30e1ae4b9e3fbc8d1c7befb3820777bf17dd2eb
YOUR NAME <erinpagemd@yahoo.com>
2015-01-28 18:02:23 -0600

    i have the data and need to get out of the function

--- app/js/index.js
+++ app/js/index.js
@@ -1,32 +1,28 @@
 var enteredZip = document.querySelector('#zipCode');  //zip is the value from the zip input field in form
 var zip;
-var submit = document.querySelector('.submitZip')
+var submit = document.querySelector('.submitZip');
+var url;
 
 
 submit.addEventListener('click', function(){
     zip = enteredZip.value;
-    console.log(zip);
-    debugger;
-});
+    var url = 'https://api.wunderground.com/api/e948aefbd9d71dd2/forecast/q/' + zip + '.json'
+    console.log(url);
 
+    function getJSON(url, cb) {
+      var xhr = new XMLHttpRequest();
+      xhr.open('GET', url);
 
-/* 
-var url = 'https://api.wunderground.com/api/e948aefbd9d71dd2/forecast/q/' + zip + '.json'
-
-function getJSON(url, cb) {
-  var xhr = new XMLHttpRequest();
-  request.open('GET', url);
-
-  xhr.onload = function (){
-    if (this.status >= 200 && this.status < 400) {
-      cb(JSON.parse(this.data));
+      xhr.onload = function (data){
+        //debugger;
+        if (this.status >= 200 && this.status < 400) {
+          cb(JSON.parse(this.response));
+        }
+      }
+      xhr.send();
     }
-  }
 
-  xhr.send();
-}
-
-getJSON(url, function(data) {
-
+    getJSON(url, function(data) {
+      debugger;
+    });
 });
-*/
